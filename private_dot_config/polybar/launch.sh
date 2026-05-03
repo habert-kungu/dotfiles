5434b57346f55350abe4c6fb52cc484fc128b59e
Habert Kungu <habert231@gmail.com>
2026-05-03 22:46:43 +0300

    Fix launch.sh bar name

--- private_dot_config/polybar/launch.sh
+++ private_dot_config/polybar/launch.sh
@@ -4,6 +4,6 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 echo "---" | tee -a /tmp/polybar1.log 
-polybar main 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar bar 2>&1 | tee -a /tmp/polybar1.log & disown
 
 echo "Bars launched..."
