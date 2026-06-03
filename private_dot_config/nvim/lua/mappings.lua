48e2e041b7d3d34f9d68e0c1f510a6b2bcde03eb
Habert Kungu <habert231@gmail.com>
2026-06-03 11:47:13 +0300

    Add <leader>li keymap to list active LSP clients

--- private_dot_config/nvim/lua/mappings.lua
+++ private_dot_config/nvim/lua/mappings.lua
@@ -7,4 +7,18 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+-- Show active LSP clients
+map("n", "<leader>li", function()
+  local clients = vim.lsp.get_clients { bufnr = 0 }
+  if #clients == 0 then
+    vim.notify("No LSP clients active", vim.log.levels.WARN)
+    return
+  end
+  local names = {}
+  for _, c in ipairs(clients) do
+    table.insert(names, c.name)
+  end
+  vim.notify("LSP: " .. table.concat(names, ", "), vim.log.levels.INFO)
+end, { desc = "List active LSP clients" })
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
