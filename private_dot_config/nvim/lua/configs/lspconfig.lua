2c651f948ff7fbf61bc91f1140ed4503bcc45ec2
Habert Kungu <habert231@gmail.com>
2026-06-03 10:47:41 +0300

    Minimal nvim config: 4 plugins, auto-install via mason-lspconfig

--- private_dot_config/nvim/lua/configs/lspconfig.lua
+++ private_dot_config/nvim/lua/configs/lspconfig.lua
@@ -1,24 +1,19 @@
-local lspconfig = require "lspconfig"
 local lsp_utils = require "nvchad.configs.lspconfig"
 
 local on_attach = lsp_utils.on_attach
 local on_init = lsp_utils.on_init
 local capabilities = lsp_utils.capabilities
 
--- Default LSP server setup
-local default_servers = { "html", "cssls", "gopls", "ruff", "pyright", "ts_ls", "eslint" }
+-- Servers that need no custom config
+local default_servers = { "html", "cssls", "ruff", "ts_ls", "eslint" }
 
 for _, lsp in ipairs(default_servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    on_init = on_init,
-    capabilities = capabilities,
-  }
+  vim.lsp.enable(lsp)
 end
--- setup for go
-lspconfig.gopls.setup {
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gompl" },
+
+-- gopls
+vim.lsp.config.gopls = {
+  filetypes = { "go", "gomod", "gowork", "gotmpl" },
   settings = {
     gopls = {
       completeUnimported = true,
@@ -29,50 +24,37 @@ lspconfig.gopls.setup {
     },
   },
 }
+vim.lsp.enable("gopls")
 
--- Custom HTML LSP setup
-lspconfig.html.setup {
-  opts = {
-    root_dir = function(fname)
-      return lspconfig.util.root_pattern ".git"(fname) or lspconfig.util.path.dirname(fname)
-    end,
-    filetypes = { "html", "htmldjango" },
-  },
-  on_attach = on_attach, -- Add on_attach for consistency
-  on_init = on_init,
-  capabilities = capabilities,
-}
-
--- Custom Python LSP setup (Pyright)
-lspconfig.pyright.setup {
+-- pyright
+vim.lsp.config.pyright = {
   settings = {
     pyright = {
-      disableOrganizeImports = true, -- Using Ruff's import organizer
+      disableOrganizeImports = true,
     },
     python = {
       analysis = {
-        ignore = { "*" }, -- Exclusively use Ruff for linting
+        ignore = { "*" },
       },
     },
   },
-  on_attach = on_attach, -- Add on_attach for consistency
-  on_init = on_init,
-  capabilities = capabilities,
 }
+vim.lsp.enable("pyright")
 
--- Custom Ruff LSP on_attach
-local function custom_ruff_on_attach(client, bufnr)
-  if client.name == "ruff_lsp" then
-    client.server_capabilities.hoverProvider = false -- Disable hover in favor of Pyright
-  end
-  on_attach(client, bufnr) -- Call the original on_attach
-end
+-- html with custom filetypes
+vim.lsp.config.html = {
+  filetypes = { "html", "htmldjango" },
+}
+vim.lsp.enable("html")
 
--- Custom Clangd LSP setup
-lspconfig.clangd.setup {
+-- clangd with custom on_attach
+vim.lsp.config.clangd = {
   on_attach = function(client, bufnr)
     client.server_capabilities.signatureHelpProvider = false
-    custom_ruff_on_attach(client, bufnr) -- Use custom ruff on_attach
+    if client.name == "ruff" then
+      client.server_capabilities.hoverProvider = false
+    end
+    on_attach(client, bufnr)
   end,
-  capabilities = capabilities,
 }
+vim.lsp.enable("clangd")
--- private_dot_config/nvim/lua/plugins/init.lua
+++ private_dot_config/nvim/lua/plugins/init.lua
@@ -1,98 +1,50 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = "BufWritePre", -- uncomment for format on save
-		config = function()
-			require("configs.conform")
-		end,
-	},
+  -- Mason: installs LSP servers, formatters, linters
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    opts = {
+      ensure_installed = {
+        "stylua", "prettierd", "black", "isort", "clang-format",
+      },
+    },
+  },
 
-	-- LeetCode plugin
-	{
-		"kawre/leetcode.nvim",
-		build = ":TSUpdate html",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-		},
-		opts = {
-			-- configuration goes here
-		},
-	},
-	{ "wakatime/vim-wakatime", lazy = false },
+  -- mason-lspconfig: auto-install + integrate with nvim-lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require("mason-lspconfig").setup({ automatic_installation = true })
+      require("configs.lspconfig")
+    end,
+  },
 
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("nvchad.configs.lspconfig").defaults()
-			require("configs.lspconfig")
-		end,
-	},
-	--Mason
-	{
-		"mason-org/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"lua-language-server",
-				"stylua",
-				"html-lsp",
-				"css-lsp",
-				"pyright",
-				"black",
-				"debugpy",
-				"mypy",
-				"ruff",
-				"ruff-lsp",
-				"pyright",
-				"clangd",
-				"clang-format",
-				"eslint-lsp",
-				"prettierd",
-				"tailwindcss-language-server",
-				"ts_ls",
-			},
-		},
-	},
-	--treesitter
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		opts = {
-			indent = { enable = true },
-			highlight = { enable = true },
-			auto_install = true,
-		},
-	},
-	--nvim-lint for linting
-	{
-		"nvimtools/none-ls.nvim",
-		event = "VeryLazy",
-		opts = function()
-			return require("configs.null-ls")
-		end,
-	},
-	{
-		"windwp/nvim-ts-autotag",
-		ft = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-		config = function()
-			require("nvim-ts-autotag").setup()
-		end,
-	},
-	{
-		"christoomey/vim-tmux-navigator",
-		cmd = {
-			"TmuxNavigateLeft",
-			"TmuxNavigateDown",
-			"TmuxNavigateUp",
-			"TmuxNavigateRight",
-			"TmuxNavigatePrevious",
-		},
-		keys = {
-			{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
-			{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
-			{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
-			{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
-			{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
-		},
-	},
+  -- Treesitter: syntax highlighting
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    opts = {
+      indent = { enable = true },
+      highlight = { enable = true },
+      auto_install = true,
+    },
+  },
+
+  -- Tmux navigator: seamless pane switching
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft", "TmuxNavigateDown", "TmuxNavigateUp",
+      "TmuxNavigateRight", "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 }
