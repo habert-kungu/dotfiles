6b29e54ee24655d7f05627d5014f7fbcb87b4869
Habert Kungu <habert231@gmail.com>
2026-07-13 17:38:53 +0300

    Fix image preview: defer terminal command until split is ready

--- private_dot_config/nvim/lua/options.lua
+++ private_dot_config/nvim/lua/options.lua
@@ -5,16 +5,25 @@ require "nvchad.options"
 -- Use system clipboard by default (yank/delete/paste sync with + register)
 vim.opt.clipboard = "unnamedplus"
 
--- Preview images inline in a terminal split (uses WezTerm/Ghostty Kitty protocol)
-vim.api.nvim_create_user_command("Img", function()
-  local file = vim.fn.expand("%:p")
-  if vim.fn.executable("wezterm") == 1 then
-    vim.cmd("below split | terminal wezterm imgcat " .. vim.fn.shellescape(file))
-  else
-    vim.cmd("below split | terminal viu " .. vim.fn.shellescape(file))
-  end
-end, {})
-vim.keymap.set("n", "<leader>i", "<cmd>Img<cr>", { desc = "Preview image" })
+-- Auto-preview images when opening image files (uses WezTerm Kitty protocol)
+vim.api.nvim_create_autocmd("BufRead", {
+  group = vim.api.nvim_create_augroup("ImagePreview", { clear = true }),
+  pattern = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.bmp", "*.svg", "*.ico" },
+  callback = function(args)
+    local file = vim.fn.expand("%:p")
+    vim.api.nvim_buf_delete(args.buf, { force = true })
+    vim.cmd("below split | terminal")
+    local cmd = vim.fn.executable("wezterm") == 1
+      and ("wezterm imgcat " .. vim.fn.shellescape(file) .. " && read -p ''")
+      or ("viu " .. vim.fn.shellescape(file) .. " && read -p ''")
+    vim.defer_fn(function()
+      local chan = vim.bo[vim.api.nvim_get_current_buf()].channel
+      if chan and chan > 0 then
+        vim.fn.chansend(chan, cmd .. "\n")
+      end
+    end, 200)
+  end,
+})
 
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:block"
 
